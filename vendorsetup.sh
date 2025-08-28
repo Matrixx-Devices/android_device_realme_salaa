ffe26843dabbaec6ead613e1c11ebfa744c4fa16
Ansh <singhansh64321@gmail.com>
2025-08-29 00:10:46 +0530

    salaa: vendorsetup: Updated few dependencies
    * Also added signing keys
    
    Change-Id: I26b9b9cc310942efa529b9bf41d8166ef59571dd

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,21 +1,21 @@
 echo 'Starting to clone stuffs needed for your device'
-echo 'Cloning Vendor tree [1/5]'
+echo 'Cloning Vendor tree [1/6]'
 # Vendor
 rm -rf vendor/realme/salaa
 if [ ! -d "vendor/realme/salaa" ]; then
     mkdir -p vendor/realme/salaa
-    git clone https://github.com/PixelOS-Devices/vendor_realme_salaa.git --depth=1 -b fifteen vendor/realme/salaa
+    git clone https://github.com/crdroidandroid/proprietary_vendor_realme_salaa.git --depth=1 -b 15.0 vendor/realme/salaa
 fi
 
-echo 'Cloning Kernel tree [2/5]'
+echo 'Cloning Kernel tree [2/6]'
 # Kernel
-rm -rf kernel/realme/mt6785
-if [ ! -d "kernel/realme/mt6785" ]; then
-    mkdir -p kernel/realme/mt6785
-    git clone https://github.com/PixelOS-Devices/kernel_realme_salaa.git --depth=1 -b fifteen kernel/realme/salaa
+rm -rf kernel/realme/salaa
+if [ ! -d "kernel/realme/salaa" ]; then
+    mkdir -p kernel/realme/salaa
+    git clone https://github.com/crdroidandroid/android_kernel_realme_salaa.git --depth=1 -b 15.0 kernel/realme/salaa
 fi
 
-echo 'Cloning Mediatek SEpolicy_vndr [3/5]'
+echo 'Cloning Mediatek SEpolicy_vndr [3/6]'
 # SEpolicy
 rm -rf device/mediatek/sepolicy_vndr
 if [ ! -d "device/mediatek/sepolicy_vndr" ]; then
@@ -23,7 +23,7 @@ if [ ! -d "device/mediatek/sepolicy_vndr" ]; then
     git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git --depth=1 -b lineage-22.2 device/mediatek/sepolicy_vndr
 fi
 
-echo 'Cloning Hardware Mediatek [4/5]'
+echo 'Cloning Hardware Mediatek [4/6]'
 # Hardware Mediatek
 rm -rf hardware/mediatek
 if [ ! -d "hardware/mediatek" ]; then
@@ -31,7 +31,7 @@ if [ ! -d "hardware/mediatek" ]; then
     git clone https://github.com/LineageOS/android_hardware_mediatek.git --depth=1 -b lineage-22.2 hardware/mediatek
 fi
 
-echo 'Cloning Hardware Oplus [5/5]' 
+echo 'Cloning Hardware Oplus [5/6]' 
 # Hardware Oplus
 rm -rf hardware/oplus
 if [ ! -d "hardware/oplus" ]; then
@@ -39,4 +39,11 @@ if [ ! -d "hardware/oplus" ]; then
     git clone https://github.com/LineageOS/android_hardware_oplus.git --depth=1 -b lineage-22.2 hardware/oplus
 fi
 
+echo 'Cloning Signing Keys [6/6]' 
+# Signing Keys
+rm -rf vendor/lineage-priv
+if [ ! -d "vendor/lineage-priv" ]; then
+    mkdir -p vendor/lineage-priv
+    git clone https://github.com/Matrixx-Devices/vendor_lineage-priv.git --depth=1 -b 15.0 vendor/lineage-priv/keys
+fi
 echo 'Completed, Now proceeding to lunch'
